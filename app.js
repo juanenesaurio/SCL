cb55c39b4f165825ad27b4f16cddb2a11f7d7e11
Arquitecto de la Aguarescencia <sfla@local>
2026-03-26 23:36:31 -0600

    menú productos fondo

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Función para ocultar todas las secciones
 function ocultarTodo() {
-  ["bienvenida"].forEach(id =>
+  ["bienvenida", "productos"].forEach(id =>
     document.getElementById(id).classList.add("hidden")
   );
 }
 
-// Función para ir directo al menú (lienzo en blanco)
+// Función para ir directo al menú de productos
 function irDirectoAMenu() {
   ocultarTodo();
+  document.getElementById("productos").classList.remove("hidden");
 }
